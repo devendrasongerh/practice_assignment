b2ec63fba17f8743086789ddb962d6ceab9cd980
devendrasongerh <devendrasongerh@gmai.com>
2022-10-19 16:49:04 +0530

    correct the code

---
+++ palindrome_problem.js
@@ -0,0 +1,8 @@
+//function checkPalindrome(N, str) {
+
+let N=7;
+str=racecar;
+  let new_str ="";
+  for (let a=N-1; a>=0; a--){
+       new_str = new_str + str[a];}
+       
---
+++ prime_problem.js
@@ -0,0 +1,9 @@
+//function identifyPrime(num) {
+   
+let num=16;
+count=0;
+ for(let a=1; a<=num; a++){
+     if(num%a==0){
+     count++ ;
+     }}
+     
